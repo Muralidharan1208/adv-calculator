ad0b445132ba6af5b4ea6e7eb1a62afe428df444
Muralidharan1208 <136390511+Muralidharan1208@users.noreply.github.com>
2024-11-09 23:07:55 +0530

    changed calculator to multiplication
    changed calculator to multiplication

--- src/main/java/com/adv/calculator/service/Multiplication.java
+++ src/main/java/com/adv/calculator/service/Multiplication.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class Calculator {
+public class Multiplication {
 	
 	@GetMapping("/multiply")
 	public int multiply() {
