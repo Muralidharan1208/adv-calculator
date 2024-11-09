e862b4a42f433fb40930fff62c7fbd806a127893
Muralidharan1208 <136390511+Muralidharan1208@users.noreply.github.com>
2024-11-09 20:59:19 +0530

    my inital code for calculator
    my inital code for calculator

---
+++ src/main/java/com/adv/calculator/AdvClaculatorApplication.java
@@ -0,0 +1,13 @@
+package com.adv.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvClaculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvClaculatorApplication.class, args);
+	}
+
+}
